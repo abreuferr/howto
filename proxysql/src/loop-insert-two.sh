0b42856c1577834cb29af2c878503a25baa0f5e5
caio abreu ferreira <abreuferr@gmail.com>
2024-04-03 22:33:05 -0300

    movidos para outro projeto

--- proxysql/src/loop-insert-two.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-while true; do mysql -u superuser-test -h 192.168.122.21 -P 6033 -e 'INSERT INTO senhasegura.proxysqlTest (proxysqlTest_string) VALUES ("string")' -pcapivarasuicida; sleep 1; done
